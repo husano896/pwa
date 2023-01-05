b28004faa57a147926cc683fd50dc40fcaaa449d
xFly <husano896@gmail.com>
2023-01-05 17:37:00 +0800

    小亞可愛：修正在IOS下因為沒有movementY而不會動的問題

--- src/app/apps/asiaminor/asiaminor.component.ts
+++ src/app/apps/asiaminor/asiaminor.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 // 直向最小的縮放程度 (同時影響橫向最大縮放程度)
-const minHeightScale = 0.6;
+const minHeightScale = 0.25;
 
 @Component({
   selector: 'app-asiaminor',
@@ -16,20 +16,37 @@ export class AsiaMinorComponent implements OnInit {
   @ViewChild('asiaminor', { static: false }) awoo?: ElementRef<HTMLImageElement>
 
   private movedY = 0;
+  private lastY: number | null = null;
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  @HostListener('pointermove', ['$event'])
+
+  @HostListener('document:pointerdown', ['$event'])
+  onPointerDown(ev: PointerEvent): void {
+    this.lastY = ev.y;
+  }
+
+  @HostListener('document:pointermove', ['$event'])
   onPointerMove(ev: PointerEvent): void {
-    const deltaY = ev.movementY;
+    if (this.lastY == null) {
+      return;
+    }
+    const deltaY = ev.y - this.lastY;
     this.movedY = Math.max(0, Math.min(this.movedY + deltaY, 512))
 
     // 調整小亞的縮放程度
     const scaleX = 1 + (minHeightScale * this.movedY / 512);
     const scaleY = 1 / scaleX;
     this.awoo!.nativeElement.style.transform = `scale(${scaleX}, ${scaleY})`;
+    this.lastY = ev.y;
     ev.preventDefault();
   }
+
+  @HostListener('document:pointerup', ['$event'])
+  onPointerUp(ev: PointerEvent): void {
+    this.lastY = null;
+  }
+
 }
